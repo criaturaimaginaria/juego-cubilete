993cb49e255c5868163f00c7060ff7611a4b8447
Valentin <valkjavalkja@gmail.com>
2024-07-11 11:26:47 -0300

    lenguage switcher buttons

--- src/components/LanguageSwitcher/index.jsx
+++ src/components/LanguageSwitcher/index.jsx
@@ -1,16 +1,26 @@
 'use client';
 import { useContext } from 'react';
 import { LanguageContext } from '../../contexts/LenguageContext.js';
+import styles from './switcher.module.css';
+
 
 export const LanguageSwitcher = () => {
   const { language, switchLanguage } = useContext(LanguageContext);
 
   return (
     <div>
-      <button onClick={() => switchLanguage('en')}>
+      <button
+        onClick={() => switchLanguage('en')}
+        className={`${styles.button} ${language === 'en' ? styles.disabled : ''}`}
+        disabled={language === 'en'}
+      >
         English
       </button>
-      <button onClick={() => switchLanguage('es')}>
+      <button
+        onClick={() => switchLanguage('es')}
+        className={`${styles.button} ${language === 'es' ? styles.disabled : ''}`}
+        disabled={language === 'es'}
+      >
         Español
       </button>
     </div>
