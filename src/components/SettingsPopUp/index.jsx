3d9b74f979c703b006c018944c014d599ebe0a65
Valentin <valkjavalkja@gmail.com>
2024-07-11 10:21:19 -0300

    test component

---
+++ src/components/SettingsPopUp/index.jsx
@@ -0,0 +1,32 @@
+'use client';
+import { useContext } from 'react';
+import LanguageSwitcher from './LanguageSwitcher';
+import { LanguageContext } from '../contexts/LanguageContext';
+
+export const Header = () => {
+  const { language } = useContext(LanguageContext);
+
+  const translations = {
+    es: {
+      text1: 'texto 1 y bueno, así son las cosas',
+      text2: 'larga vida a constantino',
+      text3: 'como te va?',
+    },
+    en: {
+      text1: 'text 1 and well.. like that are bla',
+      text2: 'Long live constantine',
+      text3: 'how are you?',
+    },
+  };
+
+
+  return (
+    <div >
+      <p>text 1 {translations[language].Content}</p>
+      <p>text 2 {translations[language].about}</p>
+      <p>text 3 {translations[language].Portfolio}</p>
+    </div>
+  );
+};
+
+export default Header;
