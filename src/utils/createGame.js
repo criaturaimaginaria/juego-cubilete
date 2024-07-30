9e8b7f92a4491320e2cc4c5c34f6c062d03c7fca
Valentin <valkjavalkja@gmail.com>
2024-07-30 12:19:38 -0300

    deleted commented lines

--- src/utils/createGame.js
+++ src/utils/createGame.js
@@ -22,26 +22,3 @@ export default createGame;
 
 
 
-
-
-
-// import { db } from '../../firebase.config.js';
-// import { ref, set } from 'firebase/database';
-
-// const createGame = (gameCode, maxPlayers) => {
-//   const gameData = {
-//     maxPlayers,
-//     players: {},
-//     currentTurn: 'player1',
-//     randomNumber: Math.floor(Math.random() * 100) + 1,
-//     gameStatus: 'waiting', // waiting, in-progress, finished
-//     winner: null,
-//     gameHistory: [],
-//   };
-
-//   const gameRef = ref(db, `games/${gameCode}`);
-//   return set(gameRef, gameData);
-// };
-
-// export default createGame;
-
